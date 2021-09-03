1bb18cc48dd15ad1a62c995ea01c14508ed5014d
Zak Tinga <brndkt@gmail.com>
2021-09-03 16:03:31 +0200

    first commit and an example of firebase v9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref, set, onValue, remove } from "firebase/database";
+import { collection, addDoc, getFirestore } from "firebase/firestore";
+
+// import { getAuth,  } from "firebase/auth";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,132 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestApp';
+
+  auth: any;
+  database: any;
+  dbFirestore: any;
+  data = {
+    username: 'loading...'
+  };
+
+  bookingsList: any;
+
+  constructor() {
+    this.auth = getAuth();
+    this.database = getDatabase();
+    this.dbFirestore = getFirestore();
+
+
+    /*
+    @bookingsRef: refences the booking node or path on the Realtime Database where the 'bookings' is located.
+    */
+    const bookingsRef = ref(this.database, 'bookings');
+
+
+    /*
+    this method is respobsible for retreiving the list of bookings from the Realtime Database.
+    */
+    onValue(bookingsRef, (snapshot) => {
+      this.bookingsList = [];
+      snapshot.forEach(element => {
+        /*
+        @bookingsList: this loops through the list and pushes it to the newly created array and @Object.assign assigns the key to its object.
+        */
+        this.bookingsList.push(Object.assign(element.val(), { key: element.key }));
+        console.log(this.bookingsList);
+      });
+    });
+
+    /*
+    @onAuthStateChanged: checks whether the user is logged in or not and provides the user details in an object form called 'user'.
+    */
+    onAuthStateChanged(this.auth, user => {
+      if (user) {
+        /*
+        @userObject: It references the path to where the current logged in user details or object is located on the Realtime Database.
+        */
+        const userObject = ref(this.database, 'users/' + user.uid);
+        /*
+        @userObject: It references the path to where the current logged in user details or object is located on the Realtime Database.
+        */
+        onValue(userObject, (snapshot) => {
+          this.data = snapshot.val();
+          console.log('User Data: ', this.data);
+        });
+
+        /*
+        @addDoc: It's responsible for posting data on Firestore.
+        */
+        try {
+          const docRef = addDoc(collection(this.dbFirestore, "users"), {
+            first: "Ada",
+            last: "Lovelace",
+            born: 1815
+          });
+          console.log("Document written with ID: ", docRef);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+
+      } else {
+        console.log('No user is logged in...');
+      }
+    });
+
+    // createUserWithEmailAndPassword(this.auth, 'selepe.lk@gmail.com', '123456').then((userCredential) => {
+    //     // Signed in and navigate to your landing page
+    //     const user = userCredential.user;
+    //     console.log(user);
+
+    //     // ...
+    //   }).catch((error) => {
+    //     // remain on this page
+
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //     console.log(errorMessage);
+    //     // remain on this page
+    //     // ..
+    //   });
+
+  }
+
+
+  /*
+  @delete: Responsible for deleting data on the Realtime Database.
+  */
+  delete(key: string) {
+    console.log(key);
+    const deleteBookingRef = ref(this.database, 'bookings/' + key);
+    remove(deleteBookingRef).then(() => {
+      console.log('Dleted  Successfully!!!!!!');
+    });
+  }
+
+    
+  /*
+  @submit: Stores user information to the Realtime Database under their 'UID'.
+  */
+
+  submit() {
+    onAuthStateChanged(this.auth, user => {
+      if (user) {
+        set(ref(this.database, 'users/' + user.uid), {
+          username: this.title,
+          email: user.email,
+          profile_picture: user.photoURL,
+          phone_number: '0783849933',
+          bio: 'I\m hard working...',
+        }).then(() => {
+          console.log('Successful');
+        }).catch(er => {
+          console.log(er.message);
+        });
+
+      } else {
+        console.log('No user is logged in...');
+
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD83loPdRIt4CNxrmhUPapZ8P9BGBvt55E",
+  authDomain: "zackapp-10ae1.firebaseapp.com",
+  databaseURL: "https://zackapp-10ae1.firebaseio.com",
+  projectId: "zackapp-10ae1",
+  storageBucket: "zackapp-10ae1.appspot.com",
+  messagingSenderId: "653209157174",
+  appId: "1:653209157174:web:6a29ccf06ffffdf3"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
@@ -10,7 +30,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
